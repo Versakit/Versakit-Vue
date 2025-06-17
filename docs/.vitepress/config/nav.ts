b0cd0a92ddad2b96baeb1f07543ab7523b2b249f
Jannik <1337741710@qq.com>
2025-06-17 18:09:31 +0800

    chore: 更新导航配置，移除设计链接以简化文档结构

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -3,7 +3,6 @@ import { DefaultTheme } from 'vitepress'
 export const getNav = (): DefaultTheme.NavItem[] => {
   return [
     { text: '指南', link: '/guide/versakit/' },
-    { text: '设计', link: '/design/theme/' },
     { text: '组件', link: '/components/tabs/' },
     { text: '博客', link: '/blog/' },
   ]
