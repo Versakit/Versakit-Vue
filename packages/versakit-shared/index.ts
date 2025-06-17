e844c78935d54cd2f01e2c74ce79763ddbe4066a
Jannik <1337741710@qq.com>
2025-06-18 00:50:28 +0800

    chore: 新增@versakit/shared库，包含withInstall和headless功能，更新package.json以支持公共发布

---
+++ packages/versakit-shared/index.ts
@@ -0,0 +1,2 @@
+export * from './src/withinstall'
+export * from './src/headless'
---
+++ packages/versakit-vue/index.ts
@@ -0,0 +1,33 @@
+/**
+ *  待办事项
+ *  useTooltip
+ *  usePopover
+ *  useCheckbox
+ *  useToast
+ *  useCarousel
+ *  useSelect
+ *  useSlider
+ *  useSwitch
+ *  useTextarea
+ *  useTimePicker
+ *  useDatePicker
+ *  useDateTimePicker
+ */
+
+import { usePinInput } from './composables/PinInput/index'
+import { useDialog } from './composables/Dialog/index'
+import { useTabs } from './composables/Tabs/index'
+import { useDrawer } from './composables/Drawer/index'
+import { useTooltip } from './composables/Tooltip/index'
+import { usePopover } from './composables/Popover/index'
+import { useSwitch } from './composables/Switch/index'
+
+export {
+  usePinInput,
+  useDialog,
+  useTabs,
+  useDrawer,
+  useTooltip,
+  usePopover,
+  useSwitch,
+}
