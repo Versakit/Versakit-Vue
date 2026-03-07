a6451b5ad3964b74cde55ea01e44d09dc1d13775
resilience <547471919@qq.com>
2026-03-07 13:18:39 +0800

    fix(docs): enhanceApp add type

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,13 +4,14 @@ import './custom.css'
 import 'virtual:group-icons.css'
 import { Versakit } from '@versakit/vue'
 import '@versakit/vue/style'
+import type { App } from 'vue'
 
 import StorybookLink from '../components/StorybookLink.vue'
 
 export default {
   ...Theme,
   Layout: MyLayout,
-  enhanceApp({ app }) {
+  enhanceApp({ app }: { app: App }) {
     app.use(Versakit)
     app.component('Link', StorybookLink)
   },
